021794002b11f7c50596b23061a4b31f51c5319e
Vishnu Karthik <vishnu@socly.io>
2025-11-25 13:37:26 +0530

    Switch to HashRouter for better GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Toaster } from "@/components/ui/toaster";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
   <>
     <Toaster />
-    <BrowserRouter basename="/photomagnetfactory">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
 
